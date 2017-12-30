e9f4e5f1a1a47942c8f6e1c1da71cc9931039d10
Demian Jaehoon Lim <jhlim@infraware.co.kr>
2017-12-30 14:08:20 +0900

    C# 읽기 전용 디렉토리 삭제 하기
    C# 읽기 전용 디렉토리 삭제 하기

---
+++ LearningCSharp/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using LearningCSharp.util;
+
+namespace LearningCSharp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string path = "c:/test";
+
+            bool isSuccess = FileUtils.deleteDirectory(path);
+            if (isSuccess) {
+                MessageBox.Show(path + " 삭제 완료");
+            }
+            else {
+                MessageBox.Show(path + " 에 폴더가 없습니다.");
+            }
+        }
+    }
+}
---
+++ LearningCSharp/util/FileUtils.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * @brief : DirectoryInfo class 를 사용하기 위함
+ */
+using System.IO;
+
+
+namespace LearningCSharp.util
+{
+    class FileUtils
+    {
+        /*
+         * @brief : 디렉토리 삭제 메소드
+         * @arg path : 디렉토리 경로
+         */
+        public static bool deleteDirectory(string path)
+        {
+            bool isDelete = false;
+            DirectoryInfo tempDirInfo = new DirectoryInfo(path);
+
+            if (tempDirInfo.Exists == true)
+            {
+                foreach (DirectoryInfo di in tempDirInfo.GetDirectories())
+                {
+                    foreach (FileInfo fi in di.GetFiles())
+                    {
+                        // 파일 속성이 ReadOnly 인 경우에 노멀로 변경 하고 삭제 한다.
+                        if ((fi.Attributes & FileAttributes.ReadOnly) > 0)
+                        {
+                            fi.Attributes = FileAttributes.Normal;
+                        }
+                    }
+                }
+                tempDirInfo.Delete(true);
+                isDelete = true;
+            }
+
+            return isDelete;
+        }
+    }
+}
